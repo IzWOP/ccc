db47687dcfe8264212ee8ba68ba5ffff9676fba6
Isaac Vazquez <izzy@irunpy.net>
2018-12-17 20:43:13 -0800

    Updated views and views

--- clutch/landing/views.py
+++ clutch/landing/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    render(request, 'index.html', [title:home])
+    return render(request, 'index.html', {'title':home})
