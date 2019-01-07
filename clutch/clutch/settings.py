a2b445a01e24be938321be36c7dcc337583414cb
Isaac Vazquez <izzy@irunpy.net>
2019-01-06 17:07:02 -0800

    added pictures and modal

--- clutch/clutch/settings.py
+++ clutch/clutch/settings.py
@@ -124,5 +124,5 @@ STATICFILES_DIRS = [
                   ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'clutch/media')
 MEDIA_URL = '/media/'
